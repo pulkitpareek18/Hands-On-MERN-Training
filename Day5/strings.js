ed10f9851740d845a0a0b3cb06230ecf59244093
Pulkit Pareek <pulkitpareek18@gmail.com>
2025-11-03 17:08:19 +0530

    While, Do While Loops & Strings

---
+++ Day5/strings.js
@@ -0,0 +1,95 @@
+// Strings
+
+// let carName1 = "Volvo XC60";  // Double quotes
+// let carName2 = 'Volvo XC60';  // Single quotes
+
+// Escape Character
+
+// let carName1 = "Volvo \"XC60\" is the best";  // Double quotes
+// console.log(carName1);
+
+
+// let carName2 = 'Volvo "XC60" is the best';  // Single quotes
+// console.log(carName2);
+
+// Using Template Literals (Backticks)
+
+// let text = `He's often called "Johnny"`;
+// console.log(text);
+
+// Multi-line Strings
+
+// let text = `The quick
+// brown fox
+// jumps over
+// the lazy dog`;
+
+// console.log(text);
+
+// let text = "The quick\nbrown fox\njumps over\nthe lazy dog";
+// console.log(text);
+
+// Template Literals
+
+// let firstName = "John";
+// let lastName = "Doe";
+
+// let introduction = "My name is " + firstName + " " + lastName + ".";
+// console.log(introduction);
+
+// let introduction2 = `My name is ${firstName} ${lastName}.`;
+// console.log(introduction2);
+
+// String Methods
+
+let str = "      Hello, welcome to the world of JavaScript.        ";
+
+// let result = str.length;  // Length of the string
+// console.log(result);
+
+// let result = str.toUpperCase();  // Convert to uppercase
+// console.log(result);
+// console.log(str);  // Original string remains unchanged, Strings are immutable
+
+// let result = str.toLowerCase();  // Convert to lowercase
+// console.log(result);
+
+// let result = str.slice(7, 14);  // Extract a part of the string
+// console.log(result);
+
+// let result = str.replace("JavaScript", "MERN");  // Replace a substring
+// console.log(result);
+// console.log(str);  // Original string remains unchanged
+
+// let result = str.includes("welcome");  // Check if substring exists
+// console.log(result);
+
+// let result = str.indexOf("world");  // Find the index of a substring
+// console.log(result);
+
+// let result = str.split(" ");  // Split the string into an array
+// console.log(result);
+
+// console.log(str)
+// let result = str.trim();  // Remove whitespace from both ends
+// console.log(result);
+
+// let result = str.charAt(15);  // Get character at a specific index
+// console.log(result);
+
+// let result = str.startsWith("Hello");  // Check if string starts with a substring
+// console.log(result);
+
+// let result = str.endsWith("JavaScript.");  // Check if string ends with a substring
+// console.log(result);
+
+// let result = str.repeat(3);  // Repeat the string multiple times
+// console.log(result);
+
+// let result = str + " Let's learn MERN stack.";  // Concatenate strings
+// let result = str.concat(" Let's learn MERN stack.");  // Concatenate strings
+// console.log(result);
+// console.log(str);  // Original string remains unchanged
+
+// let result = str.search("welcome");  // Search for a substring
+// console.log(result);

5cc590b372b249288f2d45bf1c877e258959a2d5
samikshakabra18 <samikshakabra18@gmail.com>
2025-11-05 14:30:46 +0530

    assignment

---
+++ Day6/Samiksha_assignment.js
@@ -0,0 +1,43 @@
+//    //ans 1
+//    const friends = ["Samiksha", "Shivani", "Kriti"];
+// console.log(friends);
+
+// friends.push("Ananya");
+// console.log(friends);
+
+// friends.shift();
+// console.log(friends);
+
+// console.log(friends.indexOf("Kriti"));
+//    //ans 2
+// function sumOfSquares(a, b) {
+//   return a*a + b*b;
+// }
+
+// console.log(sumOfSquares(5, 3));
+
+// console.log(sumOfSquares(4, 6));
+//     //ans 3
+//    const book = {
+//   title: "Wings of Fire",
+//   author: "A. P. J. Abdul Kalam",
+//   year: 1999,
+//   details: function() {
+//     return this.title + " by " + this.author + " (" + this.year + ")";
+//   }
+// };
+
+// console.log(book.details());
+//     //ans 4
+//     const mySet = new Set();
+
+// mySet.add("pen");
+// mySet.add("notebook");
+// mySet.add("eraser");
+// console.log(mySet);
+
+// mySet.delete("notebook");
+// console.log(mySet);
+
+// console.log(mySet.has("pen"));
+   

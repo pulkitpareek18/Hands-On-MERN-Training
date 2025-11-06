bb3327f4619815dc5c62e8c8ec704fc4a3e2cd7b
Sandeep Goswami <sandeepgoswami3129@gmail.com>
2025-11-06 14:11:54 +0530

    Sandeep-hands-on.js
    Uncommented code examples for various JavaScript concepts.

--- Day7/hands_on.js
+++ Day7/hands_on.js
@@ -1,36 +1,51 @@
-// Practice Session on all concepts covered so far
-
-// Name: 
-// Age:
-// Date:
-// Time Start:
-// Time End:
-
-// 1. Variables and Data Types
-// let name = "John Doe";
-// const age = 30;
-// var isStudent = false;
+//Variables and Data Types
+// let name = "Sandeep";
+// const age = 20;
+// var isStudent = true;
+// console.log("Name: " + name);
+// console.log("Age: " + age);
+// console.log("Is Student: " + isStudent);    
 
 
 
-// 2. Functions
+//Functions
+// function greet(name) {
+//   return "Welcome" + name + "to the jungle!";
+// }
+// console.log(greet(" Sandeep"));
 
 
 
-// 3. Control Structures (if-else, loops)
+//Control Structures (if-else, loops)
+// let age = 20;
+
+// if (age >= 18) {
+//   console.log("Congratulations!You are eligible for this role");
+// } else {
+//   console.log("Sorry!You are not eligible for this role");
+// }
+
+// for (let i = 1; i <= 5; i++) {
+//   console.log("Count: " + i);
+// }
 
 
 
-// 4. Arrays and Objects
+//Arrays and Objects
+// let bros = ["krish", "kapil", "Sandeep"];
+// console.log(bros);
+
+// let member = {
+//   name: "Abhi",
+//   age: 25,
+//   post: "Web Developer"
+// };
+// console.log(member);
 
 
 
-// 5. String Manipulation
 
-
-
-// 6. Pattern Printing
-
-
-
-// 7. Array Iteration
+//Pattern Printing
+// for (let i = 1; i <= 5; i++) {
+//   console.log("*".repeat(i));
+// }

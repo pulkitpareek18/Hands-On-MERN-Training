d0ea3096f74765d9529fd91fdd563d58716d9e55
Csakrish <>
2025-11-06 15:51:40 +0530

    Create kapil_hands_on.js

---
+++ Day7/kapil_hands_on.js
@@ -0,0 +1,59 @@
+// Practice Session on all concepts covered so far
+
+// Name: kapil bhargav
+// Age:     17
+// Date: 5/11/2025
+// Time Start:
+// Time End:
+
+// 1. Variables and Data Types
+// let Name = "kapil bhargav";
+// const age = 17;
+// var isStudent = false;
+// console.log("Name:" ,Name)
+// console.log("age:" ,age)
+// console.log("isStundent:" ,isStudent)
+
+
+// 2. Functions
+// function hi(h,b){
+//     sum=h+b
+// }
+// hi(10,10)
+// console.log(sum)
+
+
+
+
+// 3. Control Structures (if-else, loops)
+// let age = 18;
+
+// if (age >= 18) {
+//     console.log("Aap vote de sakte ho!");
+// } else {
+//     console.log("Aap abhi vote nahi de sakte.");
+// }
+
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log("Number:", i);
+// }
+
+
+
+// 4. Arrays and Objects
+
+// g=[1,2,3,4,5,6,7]
+// console.log(g[2])
+// console.log(g)
+
+
+// 5. String Manipulation
+
+
+
+// 6. Pattern Printing
+
+
+
+// 7. Array Iteration

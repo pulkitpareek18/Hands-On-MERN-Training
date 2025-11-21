2ae8f1cdc12d2b10e457758e8931d4d31700117a
Pulkit Pareek <pulkitpareek18@gmail.com>
2025-11-21 17:04:14 +0530

    BE + FE

--- TaskMaster/backend/controllers/TaskController.js
+++ TaskMaster/backend/controllers/TaskController.js
@@ -52,7 +52,7 @@ export const updateTask = async (req,res) => {
 
         const id = req.params.id;  // Params used to get ID from URL for ex: /update/:id
         // const id = req.query.id; // Query used to get ID from URL for ex: /update?id=123
-        
+
         const {title, description, status} = req.body;
 
         const updatedTask = await TaskModel.findByIdAndUpdate(id,{
@@ -69,6 +69,27 @@ export const updateTask = async (req,res) => {
     catch (error) {
         res.status(500).json({
             message: 'Error updating task',
+            error: error.message
+        });
+    }
+}
+
+export const deleteTask = async (req, res) => {
+    try{
+
+        const id = req.params.id;
+
+        const deletedTask = await TaskModel.findByIdAndDelete(id);
+
+        res.status(200).json({
+                message: "Task Deleted Successfully",
+                task: deletedTask
+        });
+
+    }
+    catch (error) {
+        res.status(500).json({
+            message: 'Error deleting task',
             error: error.message
         });
     }
